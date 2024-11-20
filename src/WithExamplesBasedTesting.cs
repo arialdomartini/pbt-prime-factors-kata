14f0c82c27e3f2905090589657496823e5bab27d
Arialdo Martini <arialdo.martini@gmail.com>
2024-11-20 01:15:36 +0100

    A dummy, passing test

--- src/WithExamplesBasedTesting.cs
+++ src/WithExamplesBasedTesting.cs
@@ -6,75 +6,12 @@ public class WithExamplesBasedTesting
 {
     private static List<int> FactorsOf(int n)
     {
-        var remainder = n;
-        var factors = new List<int>();
-
-        var divisor = 2;
-        while (remainder > 1)
-        {
-            while (remainder % divisor == 0)
-            {
-                factors.Add(divisor);
-                remainder /= divisor;
-            }
-
-            divisor++;
-        }
-        
-        return factors;
+        throw new NotImplementedException();
     }
 
     [Fact]
-    void factors_of_1()
+    void just_pass()
     {
-        Assert.Equal([], FactorsOf(1));
-    }
-
-    [Fact]
-    void factors_of_2()
-    {
-        Assert.Equal([2], FactorsOf(2));
-    }
-
-    [Fact]
-    void factors_of_3()
-    {
-        Assert.Equal([3], FactorsOf(3));
-    }
-
-    [Fact]
-    void factors_of_4()
-    {
-        Assert.Equal([2, 2], FactorsOf(4));
-    }
-
-    [Fact]
-    void factors_of_5()
-    {
-        Assert.Equal([5], FactorsOf(5));
-    }
-
-    [Fact]
-    void factors_of_6()
-    {
-        Assert.Equal([2, 3], FactorsOf(6));
-    }
-
-    [Fact]
-    void factors_of_7()
-    {
-        Assert.Equal([7], FactorsOf(7));
-    }
-
-    [Fact]
-    void factors_of_8()
-    {
-        Assert.Equal([2, 2, 2], FactorsOf(8));
-    }
-
-    [Fact]
-    void factors_of_9()
-    {
-        Assert.Equal([3, 3], FactorsOf(9));
+        Assert.True(true);
     }
 }
--- src/WithPBT.cs
+++ src/WithPBT.cs
@@ -6,47 +6,13 @@ namespace PrimeFactorsKata;
 
 internal static class PrimeNumberTestExtensions
 {
-    internal static bool IsPrime(int n) =>
-        Enumerable.Range(1, n)
-            .Where(i => !(i == 1 || i == n))
-            .All(i => n.CannotBeDividedBy(i));
 
-    private static bool CannotBeDividedBy(this int n, int i) =>
-        n % i != 0;
-
-    internal static int Multiplied(this IEnumerable<int> xs) =>
-        xs.Aggregate(1, (product, i) => product * i);
 }
 
 public class WithPBT
 {
     private static List<int> FactorsOf(int n)
     {
-        var remainder = n;
-        var factors = new List<int>();
-
-        var divisor = 2;
-        while (remainder > 1)
-        {
-            while (remainder % divisor == 0)
-            {
-                factors.Add(divisor);
-                remainder /= divisor;
-            }
-
-            divisor++;
-        }
-        
-        return factors;
-    }
-    
-    [Property]
-    bool boolean_factorization_in_prime_numbers(PositiveInt positiveNumber)
-    {
-        var n = positiveNumber.Item;
-
-        var factors = FactorsOf(n);
-
-        return factors.All(IsPrime) && factors.Multiplied() == n;
+        throw new NotImplementedException();
     }
 }
